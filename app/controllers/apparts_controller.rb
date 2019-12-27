11bf53ae7be64f7f7b48ad965be0771b92530a90
VirAlex <alexou73@gmail.com>
2019-12-27 17:39:04 +0100

    def prix ok

--- app/controllers/apparts_controller.rb
+++ app/controllers/apparts_controller.rb
@@ -1,2 +1,26 @@
 class AppartsController < ApplicationController
+
+    require 'nokogiri'
+    require 'open-uri'
+  def index
+      set_price
+  end
+
+
+
+
+
+
+private
+
+def set_price
+  @price = []
+  html_doc = Nokogiri::HTML(open('https://www.logic-immo.com/location-immobilier-paris-75,100_1/options/groupprptypesids=1/pricemin=1/pricemax=900/areamin=1/nbrooms=1,2/order=reco_seo_desc'))
+      html_doc.search('.offer-list-item .offer-price span').each do |element|
+        @price << element.text.strip
+        # puts element.attribute('href').value
+      end
+  puts @price
+end
+
 end
