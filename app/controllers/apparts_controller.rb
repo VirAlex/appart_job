29df7e96e0b123ecf73b984e23ca5a83f7f7816f
VirAlex <alexou73@gmail.com>
2019-12-27 17:56:49 +0100

    city m2

--- app/controllers/apparts_controller.rb
+++ app/controllers/apparts_controller.rb
@@ -3,7 +3,9 @@ class AppartsController < ApplicationController
     require 'nokogiri'
     require 'open-uri'
   def index
-      set_price
+      # set_price
+      # set_city
+      set_m2
   end
 
 
@@ -23,4 +25,25 @@ def set_price
   puts @price
 end
 
+def set_city
+    @city = []
+    html_doc = Nokogiri::HTML(open('https://www.logic-immo.com/location-immobilier-paris-75,100_1/options/groupprptypesids=1/pricemin=1/pricemax=900/areamin=1/nbrooms=1,2/order=reco_seo_desc'))
+      html_doc.search('.offer-list-item .offer-details-location-half span').each do |element|
+        @city << element.text.strip
+      end
+  puts @city
 end
+
+def set_m2
+    @m2 = []
+    html_doc = Nokogiri::HTML(open('https://www.logic-immo.com/location-immobilier-paris-75,100_1/options/groupprptypesids=1/pricemin=1/pricemax=900/areamin=1/nbrooms=1,2/order=reco_seo_desc'))
+      html_doc.search('.offer-list-item .offer-details-caracteristik--area span').each do |element|
+        @m2 << element.text.strip
+      end
+end
+
+
+end
+
+
+offer-details-caracteristik--area
